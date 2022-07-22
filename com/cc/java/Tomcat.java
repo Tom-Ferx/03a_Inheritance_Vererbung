1c2fccbdbc0ee0fbff275ea3432da2f2f9004223
Peter <schmidt.multimedia@gmail.com>
2022-07-22 13:09:40 +0200

    Cat() / Tomcat() / SuperCat()

--- com/cc/java/Tomcat.java
+++ com/cc/java/Tomcat.java
@@ -1,27 +1,9 @@
 package com.cc.java;
 
-public class Tomcat {
+public class Tomcat extends SuperCat{
     
-    private String name;
-    private String furColor;
-    private int age;
-
     public Tomcat(String name, String furColor, int age) {
-        this.name = name;
-        this.furColor = furColor;
-        this.age = age;
-    }
-
-    /** Getter */
-    public String getStringAttributes(String flag) {
-        switch (flag) {
-            case "#name":
-                return name; 
-            case "#furColor":
-                return furColor; 
-            default:
-                return "ERROR: ???";
-        }
+        super(name, furColor, age);
     }
 
     public String getAge() {
